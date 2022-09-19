f2e4d12239e0058ce31bbe4cc013ce1dca2be032
Sreenath <sreenathkumar66@gmail.com>
2022-09-19 21:32:22 +0600

    fix build failed command

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,5 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [require("@tailwindcss/forms"), require('tailwind-scrollbar'), require("daisyui")],
-    variants: {
-        scrollbar: ['rounded']
-    }
+    plugins: [require("@tailwindcss/forms")],
 };
